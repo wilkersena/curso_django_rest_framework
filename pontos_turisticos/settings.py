12cd8b145ea11f8cdfe48df1753ac18a55703901
Analista 2 <analista2@inoveagora.local>
2020-01-13 16:45:31 -0300

    Commit para o Heroku

--- pontos_turisticos/settings.py
+++ pontos_turisticos/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['https://curso-api-django.herokuapp.com/']
+ALLOWED_HOSTS = ['curso-api-django.herokuapp.com','localhost:8000']
 
 
 # Application definition
