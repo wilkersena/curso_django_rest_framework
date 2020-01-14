e04887b7ffb4b14f2ea7653b943a95ced189f85d
Analista 2 <analista2@inoveagora.local>
2020-01-14 17:20:22 -0300

    Sobrescrevi a função create para aceitar nest fields

--- addresses/models.py
+++ addresses/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Address(models.Model):
-    line1 = models.CharField(max_length=150)
+    line1 = models.CharField(max_length=150, null=True, blank=True)
     line2 = models.CharField(max_length=150, null=True, blank=True)
-    city = models.CharField(max_length=150)
-    state = models.CharField(max_length=150)
+    city = models.CharField(max_length=150, null=True, blank=True)
+    state = models.CharField(max_length=150, null=True, blank=True)
     
     def __str__(self):
         return self.line1
--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,22 +1,37 @@
 from rest_framework.serializers import ModelSerializer
 from rest_framework.fields import SerializerMethodField
 from core.models import PontoTuristico
+from resources.models import Resource
 from resources.api.serializers import ResourceSerializer
 from comments.api.serializers import CommentSerializer
 from reviews.api.serializers import ReviewSerializer
 from addresses.api.serializers import AddressSerializer
+from addresses.models import Address
 from datetime import date
 
 class PontoTuristicoSerializer(ModelSerializer):
     resource = ResourceSerializer(many=True)
-    comments = CommentSerializer(many=True)
-    reviews = ReviewSerializer(many=True)
+    #comments = CommentSerializer(many=True)
+    #reviews = ReviewSerializer(many=True)
     address = AddressSerializer()
-    data_hoje = SerializerMethodField()
+    data_hoje = SerializerMethodField(read_only=True)
     class Meta:
         model = PontoTuristico
-        
+        read_only_fields = ('comments', 'reviews')
         fields = ('id','data_hoje','name','photo','description','approved','resource', 'comments', 'reviews', 'address',)
 
     def get_data_hoje(self,obj):
         return date.today().strftime("%d/%m/%Y")
+
+    def create(self, validated_data):
+        resources = validated_data.pop('resource')
+        address = validated_data.pop('address')
+        ponto = PontoTuristico.objects.create(**validated_data)
+        for resource in resources:
+            rsc = Resource.objects.create(**resource)
+            ponto.resource.add(rsc)
+        ad = Address.objects.create(**address)
+        ponto.address = ad
+        ponto.save()
+
+        return ponto
