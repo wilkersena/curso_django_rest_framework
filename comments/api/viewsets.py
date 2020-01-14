cfc2031a82576b9470267cde78f3728bb19a7fbc
Analista 2 <analista2@inoveagora.local>
2020-01-14 15:55:44 -0300

    Adicionado controle de acesso à todas as classes

--- comments/api/viewsets.py
+++ comments/api/viewsets.py
@@ -2,6 +2,8 @@ from rest_framework.viewsets import ModelViewSet
 from comments.api.serializers import CommentSerializer
 from comments.models import Comment
 from rest_framework import filters
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.authentication import TokenAuthentication
 
 class CommentViewSet(ModelViewSet):
         queryset = Comment.objects.all()
@@ -9,3 +11,5 @@ class CommentViewSet(ModelViewSet):
         filter_backends = [filters.SearchFilter]
         search_fields = ['date','approved']
         filter_fields = ['date']
+        permission_classes = [IsAuthenticatedOrReadOnly]
+        authentication_classes = [TokenAuthentication]
